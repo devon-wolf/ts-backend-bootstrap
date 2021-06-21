b94a3e1fb026dd57a479e8ded19e994925785884
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-20 19:37:38 -0700

    Update paths in test file, add note

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,9 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
+import pool from '../dist/lib/utils/pool.js';
+import setup from '../dist/data/setup.js';
 import request from 'supertest';
-import app from '../lib/app.js';
+import app from '../dist/lib/app.js';
 
+// test file is not currently set up for TypeScript, it will be directed to the /dist folder that contains the transpiled js
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
